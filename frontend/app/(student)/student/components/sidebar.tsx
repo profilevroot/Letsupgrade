172d35479a276a40b4b941084f9e30b7bd5762f9
KANAGARAJ <kanagarajvsr@gmail.com>
2025-03-16 21:37:06 +0530

    side nav link added

--- frontend/app/(student)/student/components/sidebar.tsx
+++ frontend/app/(student)/student/components/sidebar.tsx
@@ -1,38 +1,36 @@
-import { DashboardNav } from "@/components/dashboard-nav";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { navItems } from "@/constants/data";
-import { serverSession } from "@/lib/session";
 import { cn } from "@/lib/utils";
-import { Home, History, LayoutDashboard, RefreshCw } from "lucide-react";
+import Link from "next/link";
 
 export default async function Sidebar() {
-  const session = await serverSession();
-  //console.log("navItem===>",session)
-  const filteredNavItems = navItems.filter((navItem) => {
-    //console.log("navItem",navItem)
-    return session?.routes.includes(navItem?.href);
-  });
   return (
     <ScrollArea className=" h-[calc(100vh)] bg-transparent shadow-xl ">
       <aside
-        className={cn(
-          `w-20 p-8  h-screen  bg-primary text-primary-foreground`
-        )}
+        className={cn(`w-20 p-8  h-screen  bg-primary text-primary-foreground`)}
       >
+        
         <nav className="flex flex-col items-center justify-between h-full">
-          <Button className="text-4xl" variant="ghost">
-            🏠
-          </Button>
+          <Link href="/">
+            <Button className="text-4xl" variant="ghost">
+              🏠
+            </Button>
+          </Link>
+          <Link href="/student/category">
           <Button className="text-4xl" variant="ghost">
             📊
           </Button>
+          </Link>
+          <Link href="/">
           <Button className="text-4xl" variant="ghost">
             📜
           </Button>
+          </Link>
+          <Link href="/">
           <Button className="text-4xl" variant="ghost">
             🔄
           </Button>
+          </Link>
         </nav>
       </aside>
     </ScrollArea>
